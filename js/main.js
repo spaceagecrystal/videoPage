1c98834a90bc66a79f2af51f8bf53bcff1311944
Space Age Crystal <mitch@skymachine.com>
2013-08-22 13:50:36 -0700

    dynamic resizing of maincontainer div based on video size

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 
 $( document ).ready(function() {
+
 	$('video,audio').mediaelementplayer(/* Options */);
+
+	$('.Absolute-Center').css( "width", function( index ) {
+		return $('.thevideo').width();
+	});
+
+	$('.Absolute-Center').css( "height", function( index ) {
+		return $('.thevideo').height();
+	});
+
 });
